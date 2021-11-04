4d3a8238d8298c84e6eaace690afed4106f2ad60
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2021-11-04 19:05:11 +0900

    추가 : 모듈

---
+++ index.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import useSWR, { mutate } from 'swr';
+
+export function fetcher(url: string) {
+  return axios.get(url).then(response => response.data);
+}
+
+export function prefetch(url: string) {
+  return mutate(url, fetcher(url));
+}
+
+export default function useFetch(url: string) {
+  return useSWR(url, fetcher, { refreshInterval: 1000 });
+}
